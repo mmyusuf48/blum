919f2e69eca28800faad669e24ccd821bd2ec866
adamadrn <61740837+adamadrn@users.noreply.github.com>
2024-09-06 08:28:28 +0700

    Update blum.py
    fix url

--- blum.py
+++ blum.py
@@ -168,7 +168,7 @@ def get_new_token(query_id):
     data = json.dumps({"query": query_id})
 
     # URL endpoint
-    url = "https://gateway.blum.codes/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP"
+    url = "https://user-domain.blum.codes/api/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP"
 
     # Mencoba mendapatkan token hingga 3 kali
     for attempt in range(3):
